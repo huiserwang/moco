88c54a04125199093367bccac20c071dbb96756d
Huiser.Wang <wangxuehui@sjtu.edu.cn>
2022-01-18 15:48:31 +0800

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup python -u main_moco.py /workspace/dataset/imageNet1k --resume ./checkpoint_0037.pth.tar -a resnet50 --lr 0.015 --batch-size 128 --dist-url tcp://localhost:10086 --world-size 1 --rank 0 --multiprocessing-distributed --moco-t 0.2 --mlp --cos --aug-plus > train_log2.log 2>&1 &
+nohup python -u main_moco.py /workspace/dataset/imageNet1k --resume ./ckpt/checkpoint_0037.pth.tar -a resnet50 --lr 0.015 --batch-size 128 --dist-url tcp://localhost:10086 --world-size 1 --rank 0 --multiprocessing-distributed --moco-t 0.2 --mlp --cos --aug-plus > ./logs/train_log.log 2>&1 &
